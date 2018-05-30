350b3893766ac4baee950f079de35782ad1bed1b
nithink1234 <nithink1234@gmail.com>
2018-05-30 16:55:40 -0600

    ....

--- Arrays/src/AP_maxSubArrayOfNonNegativeIntegers.java
+++ Arrays/src/AP_maxSubArrayOfNonNegativeIntegers.java
@@ -29,20 +29,19 @@ public class AP_maxSubArrayOfNonNegativeIntegers {
 	
 	static int longestsubarray(int[] nums) {
 		
-		int sum =0; 
+		int start =0; 
+		
 		int max = 0;
 		
 		for(int i=0; i< nums.length; i++) {
 			
-			if(nums[i]>0) {
+			if((i==0 && nums[i]> 0) || (nums[i]>0 && nums[i-1]<0)) {
 				
-				sum += nums[i];
-				
-				if(sum> max)
-					max = sum;
+				start = i;
 			}
 			else {
-				sum = 0;
+				if(i-start> max)
+					max = i-start;
 			}
 		}
 		
